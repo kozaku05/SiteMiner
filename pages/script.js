26b7b11d1635c7994dd8b9fc191b7f05ef09d3d4
kozaku05 <kozaku0505@gmail.com>
2024-11-07 01:52:29 +0900

    完成

---
+++ pages/script.js
@@ -0,0 +1,48 @@
+const target = window.location.href;
+let getImg = document.getElementById("getImg");
+let button = document.querySelector("button");
+function submit() {
+   
+  button.style.display = "none";
+  getImg.innerHTML = "";
+  let url = document.getElementById("url").value;
+  if (!/^https?:\/\//i.test(url)) {
+    url = 'https://' + url;
+  }
+  if (url) {
+    fetch(target, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: [url],
+    })
+      .then((response) => {
+        if (response.ok) {
+          console.log("response: ok");
+          return response.json();
+        } else {
+          console.error("Failed to fetch data:", response.status);
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        for (let i = 0; i < data.length; i++) {
+          let img = document.createElement("img");
+          img.src = data[i];
+          getImg.appendChild(img);
+          button.style.display = "block";
+        }
+        if (data.length === 0) {
+          let message = document.createElement("h2");
+          message.innerHTML = "画像が見つかりませんでした。";
+          getImg.appendChild(message);
+          button.style.display = "block";
+        }
+      })
+      .catch((err) => {
+        console.error("Error:", err);
+        button.style.display = "block";
+      });
+  }
+}
---
+++ server.js
@@ -0,0 +1,65 @@
+const http = require("http");
+const fs = require("fs");
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const port = 80;
+
+const server = http.createServer((req, res) => {
+  if (req.method === "GET") {
+    if (req.url.endsWith("/")) req.url += "index.html";
+    fs.readFile(`pages/${req.url}`, (err, data) => {
+      if (err) {
+        res.writeHead(404, { "Content-type": "text/html" });
+        res.end("<h1>404 Not Found</h1>");
+      } else {
+        res.writeHead(200);
+        res.end(data);
+      }
+    });
+  }
+  if (req.method === "POST") {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+    req.on("end", () => {
+      async function get() {
+        let img = [];
+        try {
+          const response = await axios.get(body);
+          let $ = cheerio.load(response.data);
+          $("img").each((index, element) => {
+            let src = $(element).attr("src");
+            if (src) {
+              if (src.startsWith("http")) {
+                img.push(src);
+              } else {
+                img.push(body + src);
+              }
+            }
+          });
+          return img;
+        } catch (err) {
+          console.log(err);
+          return img;
+        }
+      }
+      async function plus() {
+        let getData = await get();
+        if (getData.length === 0) {
+          console.log("データがありませんでした");
+          res.end(JSON.stringify(getData));
+        } else {
+          console.log("画像URL", getData);
+          res.end(JSON.stringify(getData));
+        }
+      }
+      plus();
+    });
+  }
+});
+
+server.listen(port, () => {
+  console.log("Server runnning at port:" + port);
+});
